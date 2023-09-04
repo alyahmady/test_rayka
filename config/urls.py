12c7624ba8271a6454d35cea89b48ce711a8dd77
alyahmady <better.aly.ahmady@gmail.com>
2023-09-04 18:36:15 +0200

    feat: add Device and DeviceModel API routes

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        f"{settings.API_PREFIX}/devices/",
+        include("apps.devices.api.v1.urls", namespace="devices-api"),
+    ),
 ]

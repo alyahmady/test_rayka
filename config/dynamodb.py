2c1294f7ae4283d800dae8974b21dc1b66c5a512
alyahmady <better.aly.ahmady@gmail.com>
2023-10-02 08:40:46 +0330

    feat: add `_init_table` and `delete_table` methods to `BaseTable` DynamoDB

--- config/dynamodb.py
+++ config/dynamodb.py
@@ -3,7 +3,6 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 from django.conf import settings
-from mypy_boto3_dynamodb.service_resource import Table
 
 logger = logging.getLogger(settings.LOGGER_NAME)
 
@@ -29,8 +28,20 @@ class DynamoDBResource:
 
 
 class BaseTable:
-    _table: Table = None
+    _table = None
     _table_name: str = None
+
+    @classmethod
+    def _init_table(cls):
+        table_name = (
+            cls._table_name
+            if (not settings.TESTING)
+            else f"{cls._table_name}_testing"
+        )
+
+        if not cls._table or cls._table.name != table_name:
+            resource = DynamoDBResource()
+            cls._table = resource.Table(table_name)
 
     @classmethod
     def create_table(cls: "BaseTable") -> None:
@@ -40,28 +51,56 @@ class BaseTable:
         if not cls._table_name:
             raise NotImplementedError("`_table_name` must be defined in subclass")
 
-        # Must
         if not cls._table_name.startswith(settings.PROJECT_KEY):
-            raise ValueError(F"`_table_name` must start with `{settings.PROJECT_KEY}_`")
+            raise ValueError(f"`_table_name` must start with `{settings.PROJECT_KEY}_`")
 
         client = DynamoDBClient()
-        # TODO Retry, MaxAttempts and CapacityUnits should be configurable (not magic)
+
+        table_name = (
+            cls._table_name if (not settings.TESTING) else f"{cls._table_name}_testing"
+        )
+
         try:
             client.create_table(
-                TableName=cls._table_name,
-                AttributeDefinitions=[{"AttributeName": cls.id, "AttributeType": "S"}],
+                TableName=table_name,
+                AttributeDefinitions=[{"AttributeName": cls.id, "AttributeType": "N"}],
                 KeySchema=[{"AttributeName": cls.id, "KeyType": "HASH"}],
                 ProvisionedThroughput={
-                    "ReadCapacityUnits": 5,
-                    "WriteCapacityUnits": 5,
+                    "ReadCapacityUnits": settings.DDB_TABLE_READ_CAPACITY_UNITS,
+                    "WriteCapacityUnits": settings.DDB_TABLE_WRITE_CAPACITY_UNITS,
                 },
             )
             client.get_waiter("table_exists").wait(
-                TableName=cls._table_name,
+                TableName=table_name,
                 WaiterConfig={"Delay": 5, "MaxAttempts": 10},
             )
 
         except ClientError as exc:
             if exc.response["Error"]["Code"] != "ResourceInUseException":
                 logger.exception("Error creating table", exc_info=exc)
+                raise exc
+
+    @classmethod
+    def delete_table(cls):
+        if not settings.TESTING:
+            raise NotImplementedError("This method is only for testing")
+
+        if not cls._table_name:
+            raise NotImplementedError("`_table_name` must be defined in subclass")
+
+        if not cls._table_name.startswith(settings.PROJECT_KEY):
+            raise ValueError(f"`_table_name` must start with `{settings.PROJECT_KEY}_`")
+
+        client = DynamoDBClient()
+
+        table_name = f"{cls._table_name}_testing"
+        try:
+            client.delete_table(TableName=table_name)
+            client.get_waiter("table_not_exists").wait(
+                TableName=table_name, WaiterConfig={"Delay": 5, "MaxAttempts": 10}
+            )
+
+        except ClientError as exc:
+            if exc.response["Error"]["Code"] != "ResourceNotFoundException":
+                logger.exception("Error deleting table", exc_info=exc)
                 raise exc
